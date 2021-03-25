3842c795a3c1206f47569d42e8b05041cea4fd49
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2021-03-25 16:23:12 +0000

    added validation to person model

--- src/app.js
+++ src/app.js
@@ -3,20 +3,17 @@ const { Person } = require("./models/Person");
 const { Task } = require("./models/Task");
 
 const app = async () => {
-  //   const person = new Person({
-  //     name: "Ben",
-  //     email: "ben@email.com",
-  //     age: 23,
-  //     likesPizza: true,
-  //   });
-  //   await person.save();
-
-  const task = new Task({
-    todo: "Buy some milk",
-    completed: false,
-  });
-
-  await task.save();
+  try {
+    const person = new Person({
+      name: "James",
+      email: "james@email.com",
+      age: 23,
+      likesPizza: true,
+    });
+    await person.save();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 app();
--- src/models/Person.js
+++ src/models/Person.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Invalid Email");
+      }
+    },
   },
   age: {
     type: Number,
