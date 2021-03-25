c484360862ddda82cafd41044cf969031d685c26
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2021-03-25 15:58:43 +0000

    added task model

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
 require("./db/connection");
 const { Person } = require("./models/Person");
+const { Task } = require("./models/Task");
 
 const app = async () => {
-  const person = new Person({
-    name: "Ben",
-    email: "ben@email.com",
-    age: 23,
-    likesPizza: true,
+  //   const person = new Person({
+  //     name: "Ben",
+  //     email: "ben@email.com",
+  //     age: 23,
+  //     likesPizza: true,
+  //   });
+  //   await person.save();
+
+  const task = new Task({
+    todo: "Buy some milk",
+    completed: false,
   });
-  await person.save();
+
+  await task.save();
 };
 
 app();
