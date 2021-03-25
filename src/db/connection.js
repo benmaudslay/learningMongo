3fb1a2505903c3477b68aaeec368ae37ecf48f78
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro.local>
2021-03-25 14:28:44 +0000

    added mongoose connection

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,14 @@
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
 
 const URI =
   "mongodb+srv://Ben:flapjacks2@gql-ninja-demo.dblxo.mongodb.net/cactusdb?retryWrites=true&w=majority";
 
 const connection = async () => {
   try {
-    const client = new MongoClient(URI, { useUnifiedTopology: true });
-    await client.connect();
+    await mongoose.connect(URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
     console.log("successfully connected to catcusdb");
   } catch (error) {
     console.log(error);
